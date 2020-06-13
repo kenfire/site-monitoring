d16a9bdfe5c7145f3cb43255346c71ba9dbd94fb
kenfire <11364148+kenfire@users.noreply.github.com>
2020-06-13 22:35:30 +1000

    Restructure code into OOP

---
+++ monitor/monitor.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Copyright 2020, Kenzo Hosomi
+#
+# This file is under the GNU General Public License Version 3.0.
+# See the file `LICENSE` for details.
+import requests
+
+
+class Monitor(object):
+    def __init__(self, url):
+        self.url = url
+        self.http_status = None
+        self.response_time = None
+        self.page_content = None
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            'Pragma': 'no-cache'
+        }
+
+    def __repr__(self):
+        return 'Monitor()'
+
+    def __str__(self):
+        return str(self.__class__) + ": " + str(self.__dict__)
+
+    def fetch(self):
+        try:
+            print('Processing... ', self.url)
+            response = requests.get(self.url, headers=self.headers)
+
+        except Exception as e:
+            print('Exception occured while fetching data')
+            print(str(e))
+
+        finally:
+            self.http_status = response.status_code
+            self.response_time = response.elapsed.total_seconds()
+            self.page_content = response.text
+
+            print(self)
--- site_monitoring.py
+++ site_monitoring.py
@@ -6,41 +6,16 @@
 # See the file `LICENSE` for details.
 
 import sys
-import requests
+from monitor import monitor
 
 
 def main():
     if sys.version_info < (3, 0):
-        print("Please use python 3 to run this program")
+        print('Please use python 3 to run this program')
 
-    fetch("https://duckduckgo.com")
-
-
-def fetch(url):
-    page_content = None
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-        'Pragma': 'no-cache'
-    }
-
-    try:
-        print('Processing... ', url)
-        response = requests.get(url, headers=headers)
-
-    except Exception as e:
-        print('Exception occured while fetching data')
-        print(str(e))
-
-    finally:
-        http_status = response.status_code
-        response_time = response.elapsed.total_seconds()
-        page_content = response.text
-
-        print('Status: {', http_status, '}', 'Response time ', response_time)
-        print(response_time)
-        print(page_content.strip())
-        return page_content
+    _monitor = monitor.Monitor('https://duckduckgo.com')
+    _monitor.fetch()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
